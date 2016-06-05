0ff4db83c44f8c496b252d8f475e86f60eb2d175
rogosher <shogerr@oregonstate.edu>
2016-06-05 05:56:34 -0700

    update

--- bin/arduboy-cli.js
+++ bin/arduboy-cli.js
@@ -6,23 +6,45 @@
   var log = require('npmlog')
   log.pause()
 
+  log.info('worked if it ends with', 'ok')
+
   var path = require('path')
   var arduboy = require('../lib/arduboy.js')
   var arduboyconf = require('../lib/config/core.js')
   var errorHandler = require('../lib/utils/error-handler.js')
 
   var configDefs = arduboyconf.defs
+  var shorthands = configDefs.shorthands
   var types = configDefs.types
   var nopt = require('nopt')
 
   log.verbose('cli', process.argv)
 
   var conf = nopt(types, shorthands)
+  arduboy.argv = conf.argv.remain
+  if (arduboy.deref(arduboy.argv[0])) arduboy.command = arduboy.argv.shift()
+  else conf.usage = true
 
+  if (conf.version) {
+    console.log(arduboy.version)
+    return
+  }
+
+  log.info('using', 'arduboy@%s', arduboy.version)
+  log.info('using', 'node@%s', process.version)
+
+  process.on('uncaughtException', errorHandler)
+
+  if (conf.usage && arduboy.command !== 'help')
+  {
+    arduboy.argv.unshift(arduboy.command)
+    arduboy.command = 'help'
+  }
+
+  conf._exit = true
   arduboy.load(conf, function (er) {
     if (er) return errorHandler(er)
-    arduboy.commands[npm.command](npm.argv, errorHandler)
+    arduboy.commands[arduboy.command](arduboy.argv, errorHandler)
   })
-
 
 })()
---
+++ cli.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+require('./bin/arduboy-cli.js')
